cd0955128c0f2eafa86a1a79631727164cf80f98
Ellie GUI <elle@Ellies-MacBook-Pro.local>
2022-05-24 14:29:02 -0700

    upload lab 1

--- labassignment1/calculator.c
+++ labassignment1/calculator.c
@@ -1,18 +1,44 @@
-//enter your name here
-//enter your email here
-#include<stdio.h>
+// Zixian Gui
+// gui.zi@northeastern.edu
+
+#include <stdio.h>
+
 int main()
 {
     int a,b,c, choice;
-    
+
     printf("Enter your choice\n");
-    printf(" 1.addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n");
-    scanf("%d",&choice);
+    printf(" 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n");
+    scanf("%d", &choice);
     
     printf("Enter a and b values");
     scanf("%d %d", &a, &b);
     
-    
-           
+    switch (choice) {
+        case 1:
+            c = a + b;
+            printf("Sum = %d\n", c);
+            break;
+
+        case 2:
+            c = a - b;
+            printf("Subtraction = %d\n", c);
+            break;
+
+        case 3:
+            c = a * b;
+            printf("Product = %d\n", c);
+            break;
+        
+        case 4:
+            c = a / b;
+            printf("Division = %d\n", c);
+            break;
+        
+        default:
+            printf("Invalid choice input\n");
+            break;
+    }
+        
     return 0;
 }
--- labassignment1/employee.c
+++ labassignment1/employee.c
@@ -2,32 +2,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*structure to hold the details of an employee*/
-typedef struct employee
-{
+
+typedef struct employee {
     int empId;
     char Name[100];
     char Designation[100];
     char Dept[10];
-
 }employee_t;
+
 /* Read the employee details using scanf*/
 void readData(int n, employee_t* s)
 {
-    
+    for (int i = 0; i < n; i++){
+        printf("\nEnter the employeeID, Name, designation and Department of Employee %d:\n", (i + 1));
+        employee_t e;
+        scanf("%d", &e.empId);
+        scanf("%s", e.Name);
+        scanf("%s", e.Designation);
+        scanf("%s", e.Dept);
+        s[i] = e;
+    }
 }
+
 /* Function to print the employee details*/
-void display(int n, employee_t * s)
+void display(int n, employee_t* s)
 {
-    
-    
+    for (int i = 0; i < n; i++){
+        printf("\nThe details of Employee %d:\n", (i + 1));
+        printf("empId: %d\n", s[i].empId);
+        printf("Name: %s\n", s[i].Name);
+        printf("designation: %s\n", s[i].Designation);
+        printf("Dept: %s\n", s[i].Dept);
+    }
 }
 
 /*----------Main Program--------*/
 int main()
-
 {
-
+    int number;
     
+    printf("Enter the number of employees: ");
+    scanf("%d", &number);
 
+    employee_t s[number];
+    readData(number, &s);
+    display(number, &s);
+
+    return 0;
 }
-
