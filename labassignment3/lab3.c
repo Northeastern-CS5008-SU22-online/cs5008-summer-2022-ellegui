ef6179a743e7b0882ba260944f1fd13368402e7d
Ellie GUI <elle@Ellies-MacBook-Pro.local>
2022-06-06 15:08:03 -0700

    upload lab3

--- labassignment3/lab3.c
+++ labassignment3/lab3.c
@@ -1,20 +1,51 @@
-//enter your name here
-//enter your email here
+//Zixian Gui
+//gui.zi@northeastern.edu
+//To perform insertion sort of integers stored in a file and display the output and store it in another file.
 
 #include <stdio.h>
-#define MAX 8
-int main()
-{
-    int arr[MAX] = {80,35,56,45,10,90,12,67};
-    int i,j,temp;
-    
+#include<stdlib.h>
+
+int main() {
+    FILE *fp;
+    int arr[10], temp;
+    int count = 0, i, j; /*count will have the total number of elements in the array*/
+
+    /*read the file*/
+    fp = fopen("Input.txt","r");
+    if(fp == NULL){
+        //if file returns NULL then can't open the file
+        printf("\n Cannot open the file \n");
+        exit(0);
+    }
+
     //insert your code here
+    while (fscanf(fp, "%d", &arr[i])!= EOF) {
+        printf("%d\n", arr[i]);
+        i++;
+        count++;
+        if (count >= 10) {
+            break;
+        }
+    }
+
+    // Insertion Sort
+    for (i = 1; i < count; i++) {
+        temp = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }    
     
-    
-    
-    
-    
-    
-    
+   // to store the output in another file
+   fp = fopen("sorted.txt", "w");
+   for (i = 0; i < count; i++) {
+       fprintf(fp, "%d\n", arr[i]);
+       printf("%d\n", arr[i]);
+   } 
+    fclose(fp);
     return 0;
 }
