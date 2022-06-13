b9f219aa6694e247cd4a5a77f7d6716b3dbb0e05
Ellie GUI <elle@Ellies-MacBook-Pro.local>
2022-06-13 15:08:11 -0700

    lab 4

--- labassignment4/lab4.c
+++ labassignment4/lab4.c
@@ -1,56 +1,89 @@
-//enter your name and email here
+// name: Zixian Gui
+// email: gui.zi@northeastern.edu
+
 #include<stdio.h>
 #include<stdlib.h>
-typedef struct node
-{
+
+
+typedef struct node{
     int data;
     struct node *next;
 }node_t;
+
 
 typedef struct list{
     struct node* head;
 }List;
 
+
 /*-----creating the nodes----------*/
-node_t* newNode(int num)
-{
- 
+node_t* newNode(int num){
+    node_t* newNode = (node_t*)malloc(sizeof(node_t));
+    if (newNode == NULL){
+        printf("Memory is not allocated\n");
+        exit(1);
+    }
+    newNode->data = num;
+    newNode->next = NULL;
+    return newNode;
 }
+
+
 /*---creating linked list----*/
 List* init_LL(){
-    
+    List* l;
+    l = (List*)malloc(sizeof(List));
+    if (l == NULL){
+        printf("Memory is not allocated\n");
+        exit(1);
+    }
+    l->head = NULL;
+    return l;
 }
+
 
 /*---Insert the nodes at the begining of the list---*/
 void insertFirst(List* l, int data){
-    
-
+    node_t* temp = newNode(data);
+    if (l->head == NULL) {
+        l->head = temp;
+        return;
+    }
+    temp->next = l->head;
+    l->head = temp;
+    return;
 }
 
+
 /*----display the output--------*/
-void display(List* l)
-{
-    node_t *temp;
-    temp=l->head;
-    while(temp!=NULL)
-    {
-        printf("%d->",temp->data);
-        temp=temp->next;
+void display(List* l){
+    node_t* temp;
+    temp = l->head;
+    while (temp != NULL){
+        printf("%d->", temp->data);
+        temp = temp->next;
     }
     printf("NULL\n");
 }
 
+
 /*-------reversing the linked list using recursion------*/
-void reverse(List* l, node_t* ptr)
-{
+void reverse(List* l, node_t* ptr){
     
-    
-    
-    
-//insert your code here
-    
-    
+    //insert your code here
+    // base case
+    if (ptr->next == NULL) {
+        l->head = ptr; 
+        return;
+    }
+    // recursively call the function
+    reverse(l, ptr->next);
+    node_t* temp = ptr->next; // initial a temp node point to ptr->next
+    // reverse
+    temp->next = ptr; 
+    ptr->next = NULL; 
 }
+
 
 /*----Free the nodes-----*/
 void freenode(List* l){
@@ -61,21 +94,20 @@ void freenode(List* l){
         free(l->head);
         l->head=temp;
     }  
-    
 }
-/*-----Main program--------------*/
-int main()
-{
-    List *list=init_LL();
-    
 
+
+/*-----Main program--------------*/
+int main(){
+    List* list=init_LL();
+    
     insertFirst(list,44);
     insertFirst(list,33);
     insertFirst(list,22);
     insertFirst(list,11);
     display(list);
     reverse(list,list->head);
-    display(list);
+    display(list); 
     
     freenode(list);
     free(list);
