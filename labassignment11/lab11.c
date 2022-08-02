2afd02e0d50d851dfcaaa1a3f8cbe0c4802aee6c
Ellie GUI <elle@Ellies-MacBook-Pro.local>
2022-08-01 23:35:38 -0700

    lab11

--- labassignment11/lab11.c
+++ labassignment11/lab11.c
@@ -1,38 +1,59 @@
-//Enter your name here
-//Enter your email here
+// name: Zixian Gui
+// email: gui.zi@northeastern.edu
+
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+
 pthread_mutex_t chopstick[5];//lock variable
 
 // Thread to be executed
-void *philosopher(void *x)
-{
- //Treat variable x as a pointer to an int and then extract the value into n
+void *philosopher(void *x) {
+    //Treat variable x as a pointer to an int and then extract the value into n
     int* a=(int*)x;
     int n=*a;
     
         /*-----Insert your code here----*/
-    
-    
+    printf("Philosopher %d is thinking.\n", n + 1);
+    int first = n;
+    int second = (n + 1) % 5;
+    pthread_mutex_lock(&chopstick[first]);
+    pthread_mutex_lock(&chopstick[second]);
+    printf("Philosopher %d is eating using chopstick[%d] and chopstick[%d].\n", n + 1, first, second);
+    pthread_mutex_unlock(&chopstick[first]);
+    pthread_mutex_unlock(&chopstick[second]);
+    printf("Philosopher %d finished eating.\n", n + 1);
+
+    sleep(1);
+    return NULL;
 }
 
 
 /*------------ Main Program---------*/
-int main()
-{
+int main() {
     
 //initializing the mutex
-    
-    
+    pthread_t threads[5];
+    int i, val[5];
+    for (i=0; i<5; i++) {
+        pthread_mutex_init(&chopstick[i], NULL);
+    }
+
 //create and run the thread
-    
+    for(i=0; i<5; i++) {
+        val[i] = i;
+        pthread_create(&threads[i], NULL, (void*)philosopher, &val[i]);
+    }
     
 //wait until all the threads are done
-    
+    for(i=0; i<5; i++) {
+        pthread_join(threads[i], NULL);
+    }
     
 // Destroying the mutex
-    
+    for (i=0; i<5; i++) {
+        pthread_mutex_destroy(&chopstick[i]);
+    }
     
     return 0;
 }
