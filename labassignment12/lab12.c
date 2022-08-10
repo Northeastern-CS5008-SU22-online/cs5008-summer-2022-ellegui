c119f182cfe525a686631c83faa016c94fcd9e3c
Ellie GUI <elle@Ellies-MacBook-Pro.local>
2022-08-09 21:31:49 -0700

    lab12

--- labassignment12/lab12.c
+++ labassignment12/lab12.c
@@ -1,26 +1,41 @@
-//enter your email here
-//enter your name here
+// name: Zixian Gui
+// email: gui.zi@northeastern.edu
 
 #include<stdio.h>
 #include<string.h>
 
-int compression(char arr[], int n, char res[]){
-    int i;
-    int count=1;//counter
-    int index=0;//to store the result
-    
-    
+int compression(char arr[], int n, char res[]) {
+    int i = 0;
+    int slow = 0;
+    int fast;
     
     //insert your code here
-    
-    
-    
-    
-    
+
+    while (slow < n){
+        fast = slow;
+
+        while (fast < n && arr[fast] == arr[slow]) {
+            fast++;
+        }
+        res[i++] = arr[slow];
+        
+        if(fast - slow > 1) {
+            int diff = fast - slow;
+            char count[100];
+            sprintf(count, "%d", diff);
+            for (int j = 0; j < strlen(count); j++){
+                res[i++] = count[j];
+            }
+        }
+        slow = fast;
+    }
+    printf("Input: %s\n", arr);
+    printf("Output: %s\n", res);
+    return i; 
 }
  
-int main()
-{
+
+int main() {
     char a[]="aaaaaaaaaaaaaabbbbcccd";
     char res[50];
     int r,n=strlen(a);//n is the size of input array
